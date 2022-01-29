e506990965e181e5d9c457caed89f056851ca242
RykuPrime <Dynasty7@outlook.com>
2022-01-29 18:11:09 -0500

    Added Header

---
+++ src/Components/Header.js
@@ -0,0 +1,71 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import {
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const useStyles = makeStyles({
+    title: {
+      flex: 1,
+      color: "gold",
+      fontFamily: "Montserrat",
+      fontWeight: "bold",
+      cursor: "pointer",
+    },
+  });
+
+  const classes = useStyles();
+
+  const navigate = useNavigate();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate("/")}
+              className={classes.title}
+              variant="h6"
+            >
+              Crypto Currency
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"EUR"}>EUR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
